bc5e7b3f719e5502a3bec36ad5f17ee69b6128d3
mamebios <bruno.mamedio.rodrigues@gmail.com>
2023-04-22 18:57:33 -0300

    Added tooltip and Topic model

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from nicegui import ui
 import asyncio
 from processing import *
 
-p = Processor()
+p = Summarizer()
+t = Topicfier()
 
 # ui.label('Resumir').classes('font-black text-2xl')
 # ui.icon('sentiment_very_dissatisfied')
@@ -28,7 +29,8 @@ with ui.tab_panels(tabs, value='Resumir'):
 
             with ui.column():
                 with ui.row().classes('no-wrap'):
-                    ui.label('Criatividade')
+                    ui.label('Temperatura(?)').tooltip('Temperatura é um hiperparâmetro que pode ser usado para controlar a aleatoriedade e criatividade do texto gerado. ' \
+                                                       'Temperaturas altas (>1.5) podem gerar textos ininteligíveis.')
                 with ui.row().classes('w-[50rem] no-wrap'):
                     slider = ui.slider(min=0, max=2, value=1, step=0.1, on_change = lambda: p.change_temperature(slider.value)).props('label')
                     ui.label().bind_text_from(slider, 'value')
@@ -45,10 +47,33 @@ with ui.tab_panels(tabs, value='Resumir'):
                         result = ui.label().classes('w-full')
 
     with ui.tab_panel('Topificar'):
-        ui.label('This is the second tab')
+        ui.label('Topificar').classes('font-black text-2xl')
+        with ui.row().classes('center no-wrap'):
+            with ui.column().classes('w-[12.6rem]'):
+                async def g():
+                    button_t.props(add='loading')
+                    await asyncio.sleep(1)
+                    result_t.set_text((textarea_t.value == '') or t.process(textarea_t.value))
+                    button_t.props(remove='loading')
+                
+            button_t = ui.button('Process', on_click = g)
+
+
+        with ui.row().classes('w-full no-wrap'):
+            with ui.column().classes('w-[38rem]'):
+                with ui.card().classes('w-full'):
+                    textarea_t = ui.textarea(label='Texto', placeholder='Insira o texto',
+                                           on_change=lambda e: None).classes('w-full').props('autogrow') #result.set_text(e.value)
+            with ui.column().classes('w-[38rem]'):
+                    with ui.card().classes('w-full'):
+                        ui.label("Resultado:\n")
+                        result_t = ui.label().classes('w-full')
+        
+
+
 
 #ui.run(title='GPT 9000', dark='true')
 #ui.run(title='Ferramenta', port=3036)
-ui.run(title='Ferramenta')
+ui.run(title='Ferramenta', port = 3036)
 
 
--- processing.py
+++ processing.py
@@ -4,11 +4,17 @@ from langchain.chains.mapreduce import MapReduceChain
 from langchain.prompts import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
 from langchain.docstore.document import Document
+from langchain.document_loaders import TextLoader
+#from langchain.chains.retrieval_qa.base import RetrievalQA
+#from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+#from langchain.vectorstores.chroma import Chroma
+from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
+from llama_index import Document as Doc
 
 key = ''
 with open('key.txt', 'r') as f: key = f.readlines()[0]
 
-class Processor:
+class Summarizer:
     def __init__(self) -> None:
 
         self.llm = OpenAI(temperature=1, openai_api_key=key)
@@ -29,3 +35,30 @@ class Processor:
 
     def get_temperature(self) -> float:
         return self.llm.temperature
+    
+class Topicfier:
+    def __init__(self) -> None:
+        #self.llm = OpenAI(temperature=1, openai_api_key=key)
+        self.query = 'What are the most important topics of this text?'
+        pass
+
+    def process(self, text: str) -> str:
+
+        doc = [Doc(text)]
+
+        index = GPTSimpleVectorIndex.from_documents(doc)
+        response = index.query(self.query)
+        print(response)
+        #return response
+        return 'ok'
+        
+        #vectorstore = Chroma().from_texts([text])
+        
+        #index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+
+        #return index.query(self.query)
+        #--------
+        #chain = RetrievalQA.from_chain_type(
+        #    self.llm, retriever=self.vectorstore.as_retriever())
+        
+        #return chain.run(self.query)
